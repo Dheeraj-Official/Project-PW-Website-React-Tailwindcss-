e79bd2ddf09939d76275c34944f80bdf64227f99
Dheeraj Kumar <dheerajmahourji@gmail.com>
2025-02-11 19:43:42 +0530

    remove unused imports and state in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import pwimage from "./assets/pwlogo.png";
 import pwcer from "./assets/pwcer.png";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
